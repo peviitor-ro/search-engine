48883f7a9094e8579b3ab2a4f3f60a1ee023ca9f
Sergiu Avram <sergiu.avram@endava.com>
2021-12-30 11:55:27 +0200

    added numpad enter

--- src/utils/searchOnEnterKeyPress.js
+++ src/utils/searchOnEnterKeyPress.js
@@ -5,7 +5,7 @@ export const searchOnEnterPress = () => {
   const inputField = document.querySelector('.search')
 
   inputField.addEventListener('keydown', (e) => {
-    if(e.code === 'Enter') {
+    if(e.code === 'Enter' || e.code === 'NumpadEnter') {
       const queryData = getUserQueryData();
       const newUrl = ceateNewUrl(queryData);
 
