a98b0dd6f7543ca36226e56d16fa579f1da83e08
snowmonkeydev <contact@sergiuavram.com>
2022-06-26 11:08:01 +0300

    added navigate to resultsg

--- src/pages/landing/landing.component.js
+++ src/pages/landing/landing.component.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import './landing.style.scss';
+
+import { useNavigate } from "react-router-dom";
+
 import Logo from '../../components/logo/logo.component';
 import ResultsCount from '../../components/results-count/results-count.component';
 import Search from '../../components/search/search.component';
-import './landing.style.scss';
 
-const LandingPage = () => (
-  <section className='landing-page'>
-    <section className='content'>
-      <Logo />
-      <Search />
-      <ResultsCount />
+const LandingPage = () => {
+  let navigate = useNavigate();
+
+  const onEnterPress = (e) => {
+    if (e.key === 'Enter') {
+      navigate("/rezultate");
+    }
+  }
+
+  return (
+    <section className='landing-page' onKeyDown={onEnterPress}>
+      <section className='content'>
+        <Logo />
+        <Search />
+        <ResultsCount />
+      </section>
     </section>
-  </section>
-);
+  )
+};
 
 export default LandingPage;
