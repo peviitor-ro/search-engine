7e654a0907c4d85c75c98b175ca93e5876e5cb65
sergiuavram <contact@sergiuavram.com>
2022-12-03 10:45:02 +0200

    rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -44,4 +44,4 @@ function App() {
 
 export default App;
 
-// https://www.figma.com/file/gY6yYTFjC0fuZ4bTRlUaH6/Pe-viitor?node-id=1029%3A1063
+// https://www.figma.com/file/gY6yYTFjC0fuZ4bTRlUaH6/Pe-viitor?node-id=1029%3A1063 
