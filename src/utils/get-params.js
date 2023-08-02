ae31f574fc47de2a50413429935164ae079d94ef
sergiuavram <contact@sergiuavram.com>
2023-08-02 10:29:50 +0300

    added encode and decode url

--- src/utils/get-params.js
+++ src/utils/get-params.js
@@ -3,11 +3,11 @@ import { queriesConst } from "../constants/queries";
 export const getQueryParams = () => {
   const params = new URLSearchParams(window.location.search);
 
-  const q = params.get(queriesConst.q) ? params.get(queriesConst.q) : '';
-  const city = params.get(queriesConst.city) ? params.get(queriesConst.city) : '';
-  const company = params.get(queriesConst.company) ? params.get(queriesConst.company) : '';
-  const country = params.get(queriesConst.country) ? params.get(queriesConst.country) : '';
-  const page = params.get(queriesConst.page) ? params.get(queriesConst.page) : 1;
+  const q = params.get(queriesConst.q) ? decodeURIComponent(params.get(queriesConst.q)) : '';
+  const city = params.get(queriesConst.city) ? decodeURIComponent(params.get(queriesConst.city)) : '';
+  const company = params.get(queriesConst.company) ? decodeURIComponent(params.get(queriesConst.company)) : '';
+  const country = params.get(queriesConst.country) ? decodeURIComponent(params.get(queriesConst.country)) : '';
+  const page = params.get(queriesConst.page) ? decodeURIComponent(params.get(queriesConst.page)) : 1;
 
   return { q, city, company, country, page }
 }
