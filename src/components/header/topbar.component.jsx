0e9d74fb9830590c20bbe85f542c8ae066c4da34
robert <robert.burcea@gmail.com>
2023-11-23 18:40:40 +0200

    implement hover on header&footer

--- src/components/header/topbar.component.jsx
+++ src/components/header/topbar.component.jsx
@@ -2,14 +2,28 @@ import React from 'react';
 import './topbar.style.scss';
 import logo from '../../assets/svgs/peviitor_logo.svg';
 import briefcase from '../../assets/svgs/briefcase.svg';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export const TopBar = ({ isBorder, resetPage }) => {
-
   return (
     <section className={`topbar ${isBorder ? 'topbar--border-bottom' : ''}`}>
-      <Link to={'/'}><img src={logo} alt='pe viitor logo' onClick={resetPage} /></Link>
-      <a href="https://www.oportunitatisicariere.ro/voluntari" target="_blank" rel='noreferrer'><img src={briefcase} alt='briefcase logo' className='icon-briefcase' /> Alătură-te</a>
+      <Link to={'/'}>
+        <img src={logo} alt="pe viitor logo" onClick={resetPage} />
+      </Link>
+      <a
+        href="https://www.oportunitatisicariere.ro/voluntari"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div className="logo-href">
+          <img
+            src={briefcase}
+            alt="briefcase logo"
+            className="icon-briefcase"
+          />{' '}
+          Alătură-te
+        </div>
+      </a>
     </section>
   );
 };
