c2abcb786d9e8d78ed0ffe368ceea1f4251d595e
robert <robert.burcea@gmail.com>
2024-01-07 16:37:51 +0200

    story-352-change-api-homepage

--- src/index.js
+++ src/index.js
@@ -11,11 +11,9 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
