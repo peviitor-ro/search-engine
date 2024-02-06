de61521e257c1e970b013e7ef666b240afa505ab
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-06 21:20:42 +0200

    Refactor topbar component and query slice
    Update topbar component to use useDispatch hook and dispatch resetQuery action. Remove unused prop.
    
    Remove updateCountry action from query slice.
    
    Add resetQuery action to query slice to reset all query parameters.

--- src/components/header/topbar.component.jsx
+++ src/components/header/topbar.component.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import './topbar.style.scss';
 import logo from '../../assets/svgs/peviitor_logo.svg';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { resetQuery } from '../../state/query.slice';
 
-export const TopBar = ({ isBorder, resetPage }) => {
+export const TopBar = ({ isBorder }) => {
+  const dispatch = useDispatch();
+
   return (
     <header className={`topbar ${isBorder ? 'topbar--border-bottom' : ''}`}>
-      <Link to={'/'}>
-        <img src={logo} alt="pe viitor logo" onClick={resetPage} />
+      <Link to={'/'} 
+        onClick={() => {
+          dispatch(resetQuery());
+        }}
+      >
+        <img src={logo} alt="pe viitor logo"/>
       </Link>
     </header>
   );
