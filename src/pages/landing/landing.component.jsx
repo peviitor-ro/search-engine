879ad35466867483db7c5870e5ef0cde2c507c04
Robert Burcea <robert.burcea@gmail.com>
2024-02-29 18:55:41 +0200

    Revert "Make changes"

--- src/pages/landing/landing.component.jsx
+++ src/pages/landing/landing.component.jsx
@@ -4,17 +4,18 @@ import { useNavigate } from 'react-router-dom';
 import { Footer } from '../../components/footer/footer.component';
 import { TopBar } from '../../components/header/topbar.component';
 import { Search } from '../../components/search/search.component';
-import { resetQuery } from '../../state/query.slice';
-import { createQueryString } from '../../utils/create-query-string';
-import { getNumberOfJobsAndCompanies } from '../../utils/get-data';
-import { Rocket } from './components/rocket/rocket.component';
-import { Title } from './components/title/title.component';
 import {
   clearJobs,
   updateTotalCompanies,
   updateTotalRomania,
   updateTotal
 } from '../../state/jobs.slice';
+import { setPageToOne, updatCity, updateCounty } from '../../state/query.slice';
+import { createQueryString } from '../../utils/create-query-string';
+import { getNumberOfJobsAndCompanies } from '../../utils/get-data';
+import { Banner } from './components/banner/banner.component';
+import { Rocket } from './components/rocket/rocket.component';
+import { Title } from './components/title/title.component';
 import './landing.style.scss';
 
 export const LandingPage = () => {
@@ -26,7 +27,9 @@ export const LandingPage = () => {
   const totalJobs = useSelector((state) => state.jobs.total);
 
   useEffect(() => {
-    dispatch(resetQuery());
+    dispatch(setPageToOne());
+    dispatch(updatCity(''));
+    dispatch(updateCounty(''));
     dispatch(clearJobs());
     getNumberOfJobsAndCompanies().then((data) => {
       dispatch(updateTotal(data.jobs.all));
@@ -34,6 +37,7 @@ export const LandingPage = () => {
       dispatch(updateTotalCompanies(data.companies));
     });
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleSearchClick = () => {
@@ -52,11 +56,12 @@ export const LandingPage = () => {
             <Search
               handleClick={handleSearchClick}
               queries={queries}
+              landing={true}
             />
           </div>
           <Rocket />
         </section>
-
+        <Banner />
       </main>
       <Footer />
     </>
--- src/state/store.js
+++ src/state/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import jobsSlice from './jobs.slice';
 import querySlice from './query.slice';
-import { locationSlice } from './location.slice';
 
 export const store = configureStore({
   reducer: {
     query: querySlice,
-    jobs: jobsSlice,
-    location: locationSlice.reducer,
+    jobs: jobsSlice
   },
 });
