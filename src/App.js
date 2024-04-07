9ebb167721bfb3059a9aa1ae58d0ada3ac1f3ac8
Calin Catuta <calinut123@yahoo.co.uk>
2024-04-07 12:07:29 +0300

    reparare problema 404 la RR

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 // Pages
 import Landing from "./pages/Landing";
 import Rezultate from "./pages/Rezultate";
 // context
 import { TagsProvider } from "./context/TagsContext";
+const router = createHashRouter([
+  {
+    path: "",
+    element: <Landing />
+  },
+  {
+    path: "*",
+    element: <Landing />
+  },
+  {
+    path: "rezultate",
+    element: <Rezultate />
+  }
+]);
 function App() {
   return (
     <TagsProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/rezultate" element={<Rezultate />} />
-        </Routes>
-      </Router>
+      <RouterProvider router={router} />
     </TagsProvider>
   );
 }
