2d363d61bc86918712e0ca342b8c5fcab729ed7e
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2024-07-18 11:36:17 +0300

    improvement of the search filter function

--- src/components/FiltreGrup.jsx
+++ src/components/FiltreGrup.jsx
@@ -8,57 +8,49 @@ import "../scss/filtre.scss";
 import { getNameOfCompanies } from "../utils/fetchData";
 import { orase } from "../utils/getCityName";
 
-// Regular expressions for replacing special characters
-const aREG = new RegExp("ș", "g");
-const bREG = new RegExp("ț", "g");
-const cREG = new RegExp("â", "g");
-const dREG = new RegExp("ă", "g");
-const eREG = new RegExp("î", "g");
-
 // CheckboxFilter component for filtering items
 const CheckboxFilter = ({ items, filterKey, searchFor, dropDown }) => {
-  const [searchQuery, setSearchQuery] = useState("");
+  // State to store the input value
+  const [inputValue, setInputValue] = useState("");
+  // State to store the filtered list of cities
+  const [filteredItems, setFilteredItems] = useState(items);
   const [error, setError] = useState(false);
   const { fields, handleCheckBoxChange } = useContext(TagsContext);
 
   // Empty the search value when dropdown its closed
   useEffect(() => {
     if (dropDown[0] || dropDown[1]) {
-      setSearchQuery("");
+      setInputValue("");
+      setFilteredItems(items);
     }
-  }, [dropDown]);
+  }, [dropDown, items]);
 
-  // Filtering items based on search query
-  const filteredItems = items.filter(
-    (item) =>
-      (searchQuery.length >= 1 &&
-        item
-          .toLowerCase()
-          .replace(aREG, "s")
-          .replace(bREG, "t")
-          .replace(cREG, "a")
-          .replace(dREG, "a")
-          .replace(eREG, "i")
-          .includes(searchQuery.toLowerCase())) ||
-      item.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  // Function to handle changes in the input field
+  const handleInputChange = (event) => {
+    const value = event.target.value; // Get the current value of the input field
+    setInputValue(value); // Update the state with the new input value
+
+    // Create a regular expression to match the input value at the start of items names
+    const regex = new RegExp(`^${normalizeString(value)}`, "i"); // Normalize the input value before creating the regex
+
+    // Filter the items array based on the normalized input value
+    const filtered = items.filter((data) => normalizeString(data).match(regex));
+
+    // Update the state with the filtered list of items
+    setFilteredItems(filtered);
+  };
+
+  // Function to normalize strings by removing diacritical marks (accents)
+  const normalizeString = (str) => {
+    // Normalize the string to decompose characters and diacritics
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    // \u0300-\u036f is a range of Unicode for diacritical marks
+  };
 
   // Effect to handle error when no results are found
   useEffect(() => {
-    setError(filteredItems.length === 0 && searchQuery.length > 0);
-  }, [filteredItems, searchQuery]);
-
-  // Displaying filtered items
-  let displayItems = filteredItems;
-
-  // Show the checked items on top
-  const checkedItems = displayItems.filter((item) =>
-    fields[filterKey].includes(item)
-  );
-  const uncheckedItems = displayItems.filter(
-    (item) => !fields[filterKey].includes(item)
-  );
-  displayItems = [...checkedItems, ...uncheckedItems.slice(0, 10)];
+    setError(filteredItems.length === 0 && inputValue.length > 0);
+  }, [filteredItems, inputValue]);
 
   return (
     <div className="search-input">
@@ -66,17 +58,17 @@ const CheckboxFilter = ({ items, filterKey, searchFor, dropDown }) => {
         <img className="lupa" src={magnifyGlass} alt="magnify-glass" />
         <input
           type="search"
-          value={searchQuery}
+          value={inputValue}
           placeholder={`Cauta ${searchFor}`}
-          onChange={(e) => setSearchQuery(e.target.value)}
+          onChange={handleInputChange}
         />
       </div>
 
       <div className="checkbox-container">
         {error ? (
-          <div>Nu există rezultate "{searchQuery}"</div>
+          <div>Nu există rezultate "{inputValue}"</div>
         ) : (
-          displayItems.map((item, index) => (
+          filteredItems.map((item, index) => (
             <div key={index} className="checkbox-parent">
               <input
                 type="checkbox"
