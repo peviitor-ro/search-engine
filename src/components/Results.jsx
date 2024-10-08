11012288a8d551db5ad4826b49dca56bd2a21cd0
Niculina <virtual_n_r@yahoo.com>
2024-10-08 17:39:02 +0300

    changed sm with md for 2 columns grid

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -121,7 +121,7 @@ const Results = () => {
       ) : (
         <>
           {jobs.length > 0 ? (
-            <section className="grid gap-7 px-14 w-full mx-auto sm:grid-cols-2 lg:flex lg:flex-wrap lg:justify-center">
+            <section className="grid gap-7 px-14 w-full mx-auto md:grid-cols-2 lg:flex lg:flex-wrap lg:justify-center">
               {jobs.map(
                 (
                   { city, company, county, job_link, job_title, remote },
