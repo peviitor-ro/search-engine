dd6053563e4b4be3c137585c47a1d64eaf2efd7c
talidag <talida.ganciu@gmail.com>
2024-11-14 10:01:43 +0200

    Changed all the letters in the footer to uppercase

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,13 +14,19 @@ const Footer = () => {
     <footer className="w-[80%] flex flex-col gap-2 border-t border-border_grey pt-[20px] mb-5">
       <section className="flex justify-between flex-wrap items-start md:!gap-1">
         <div className="flex gap-[10px] justify-between flex-wrap items-center mx-auto md:mx-0">
-          <Link to="https://www.linkedin.com/company/asociatia-oportunitati-si-cariere/"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+          <Link
+            to="https://www.linkedin.com/company/asociatia-oportunitati-si-cariere/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2"
           >
             <img src={linkedin} alt="linkedin icon" className="max-w-[100px]" />
           </Link>
-          <Link to="https://www.instagram.com/peviitor.ro"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+          <Link
+            to="https://www.instagram.com/peviitor.ro"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2"
           >
             <img
               src={instagram}
@@ -28,54 +34,86 @@ const Footer = () => {
               className="max-w-[100px]"
             />
           </Link>
-          <Link to="https://discord.gg/t2aEdmR52a"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+          <Link
+            to="https://discord.gg/t2aEdmR52a"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2"
           >
             <img src={discord} alt="discord icon" className="max-w-[100px]" />
           </Link>
-          <Link to="https://github.com/peviitor-ro"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+          <Link
+            to="https://github.com/peviitor-ro"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2"
           >
             <img src={github} alt="github icon" className="max-w-[100px]" />
           </Link>
-          <Link to="https://meet.jit.si/PEVIITOR.RO"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+          <Link
+            to="https://meet.jit.si/PEVIITOR.RO"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2"
           >
             <img src={jitsi} alt="jitsi icon" className="max-w-[100px]" />
           </Link>
-          <Link to="https://dev.to/t/peviitor/latest"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+          <Link
+            to="https://dev.to/t/peviitor/latest"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2"
           >
             <img src={dev} alt="dev icon" className="max-w-[100px]" />
           </Link>
-          <Link to="https://api.peviitor.ro/"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+          <Link
+            to="https://api.peviitor.ro/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2"
           >
             <img src={api} alt="API icon" className="max-w-[100px]" />
           </Link>
         </div>
         <div className="flex justify-between items-start gap-20 mx-auto md:mx-0">
           <div className="flex flex-col gap-1">
-            <Link to="https://oportunitatisicariere.ro/#how-contribute"
-              target="_blank" rel="noopener noreferrer"
+            <Link
+              to="https://oportunitatisicariere.ro/#how-contribute"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <strong>Implică-te!</strong>
             </Link>
 
-            <Link to="https://oportunitatisicariere.ro/#why-contribute"
-              target="_blank" rel="noopener noreferrer"
-            >Alătură-te cauzei noastre
+            <Link
+              to="https://oportunitatisicariere.ro/#why-contribute"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Alătură-te cauzei noastre
             </Link>
           </div>
 
           <div className="flex flex-col gap-1">
-            <Link to="https://oportunitatisicariere.ro/#" target="_blank" rel="noopener noreferrer">
+            <Link
+              to="https://oportunitatisicariere.ro/#"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <strong>Organizație</strong>
             </Link>
-            <Link to="https://oportunitatisicariere.ro/#team" target="_blank" rel="noopener noreferrer">
+            <Link
+              to="https://oportunitatisicariere.ro/#team"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Despre noi
             </Link>
-            <Link to="https://oportunitatisicariere.ro/#our-mission" target="_blank" rel="noopener noreferrer">
+            <Link
+              to="https://oportunitatisicariere.ro/#our-mission"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Viziune
             </Link>
           </div>
@@ -90,8 +128,12 @@ const Footer = () => {
       </section>
       <section className="flex flex-col items-center lg:flex-row mt-[20px] lg:gap-4 text-text_grey_darker">
         <p> © {year} - Toate drepturile rezervate</p>
-        <Link to="https://www.oportunitatisicariere.ro" target="_blank" rel="noopener noreferrer">
-          <strong>ASOCIAȚIA OPORTUNITĂȚi ȘI CARIERE</strong>
+        <Link
+          to="https://www.oportunitatisicariere.ro"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <strong>ASOCIAȚIA OPORTUNITĂȚI ȘI CARIERE</strong>
         </Link>
       </section>
     </footer>
