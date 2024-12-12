1a09e55d41a1352e4a745fc51de60070d2086787
AdelinaMoroaca <moroacaadelina@gmail.com>
2024-12-12 18:23:42 +0200

    Remove icon until the api site will be updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,26 @@
-// icons
+import { Link } from "react-router-dom";
 import linkedin from "../assets/svg/linkedin_icon.svg";
 import discord from "../assets/svg/discord_icon.svg";
 import github from "../assets/svg/github_icon.svg";
 import jitsi from "../assets/svg/jitsi_icon.svg";
 import instagram from "../assets/svg/instagram_icon.svg";
 import dev from "../assets/svg/dev_icon.svg";
-import api from "../assets/svg/api-svgrepo-com.svg";
-import { Link } from "react-router-dom";
+// import api from "../assets/svg/api-svgrepo-com.svg";
+
 
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
     <footer className="w-[80%] flex flex-col gap-2 border-t border-border_grey pt-[20px] mb-5">
       <section className="flex justify-between flex-wrap items-start md:!gap-1">
-        <div className="flex gap-[10px] justify-between flex-wrap items-center mx-auto md:mx-0">
+        <div className="flex gap-4 mx-auto justify-between items-center px-2 xs:mb-5 md:mx-0">
           <Link to="https://www.linkedin.com/company/asociatia-oportunitati-si-cariere/"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+            target="_blank" rel="noopener noreferrer" className="mx-1"
           >
             <img src={linkedin} alt="linkedin icon" className="max-w-[100px]" />
           </Link>
           <Link to="https://www.instagram.com/peviitor.ro"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+            target="_blank" rel="noopener noreferrer" className="mx-1"
           >
             <img
               src={instagram}
@@ -29,32 +29,32 @@ const Footer = () => {
             />
           </Link>
           <Link to="https://discord.gg/t2aEdmR52a"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+            target="_blank" rel="noopener noreferrer" className="mx-1"
           >
             <img src={discord} alt="discord icon" className="max-w-[100px]" />
           </Link>
           <Link to="https://github.com/peviitor-ro"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+            target="_blank" rel="noopener noreferrer" className="mx-1"
           >
             <img src={github} alt="github icon" className="max-w-[100px]" />
           </Link>
           <Link to="https://meet.jit.si/PEVIITOR.RO"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+            target="_blank" rel="noopener noreferrer" className="mx-1"
           >
             <img src={jitsi} alt="jitsi icon" className="max-w-[100px]" />
           </Link>
           <Link to="https://dev.to/t/peviitor/latest"
-            target="_blank" rel="noopener noreferrer" className="mx-2"
+            target="_blank" rel="noopener noreferrer" className="mx-1"
           >
             <img src={dev} alt="dev icon" className="max-w-[100px]" />
           </Link>
-          <Link to="https://api.peviitor.ro/"
+          {/* <Link to="https://api.peviitor.ro/"
             target="_blank" rel="noopener noreferrer" className="mx-2"
           >
             <img src={api} alt="API icon" className="max-w-[100px]" />
-          </Link>
+          </Link> */}
         </div>
-        <div className="flex justify-between items-start gap-20 mx-auto md:mx-0">
+        <div className="flex justify-between items-start gap-20 px-2 mx-auto md:mx-0">
         {/* <div className="flex flex-col md:flex-row justify-between items-start gap-4 md:gap-20 mx-auto md:mx-0"> */}
 
           <div className="flex flex-col gap-1">
