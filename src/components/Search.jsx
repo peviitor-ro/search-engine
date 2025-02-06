28fd9bcdaf2090179032e1bf61e9fa3a5d78ea00
Andrei Barari <b.andrei91@yahoo.com>
2025-02-06 08:54:54 +0200

    fixed landing dropdown for job title input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -68,8 +68,8 @@ const Search = () => {
   // fields
   const [text, setText] = useState("");
   // dispatch
-  const navigate = useNavigate(); 
-  const location = useLocation(); 
+  const navigate = useNavigate();
+  const location = useLocation();
   const dispatch = useDispatch();
 
   // jobs
@@ -78,13 +78,12 @@ const Search = () => {
   const nrJoburi =
     total >= 20 ? "de rezultate" : total === 1 ? "rezultat" : "rezultate";
 
-
   const [isLocation, setLocation] = useState("");
   const [focusedInput, setFocusedInput] = useState(null);
   const handleClearLocation = () => setLocation("");
   const handleFocus = (input) => setFocusedInput(input);
   // const handleBlur = () => setFocusedInput(null); // Optional, depending on whether you want to hide the dropdown when blurred
-  const [filteredCities, setFilteredCities] = useState(orase); 
+  const [filteredCities, setFilteredCities] = useState(orase);
   const dropdownRef = useRef(null);
 
   // state for job suggestion drop-down
@@ -180,7 +179,7 @@ const Search = () => {
   function handleCloseIcon() {
     setText("");
     updateUrlParams({ q: null });
-    contextSetQ(['']);
+    contextSetQ([""]);
   }
 
   // Function to filter cities based on input
@@ -269,60 +268,118 @@ const Search = () => {
     return () => clearTimeout(timer); // Cleanup timeout if user continues typing
   }, [text]);
   return (
-    <div style={{display: "flex", flexDirection:"column", justifyContent: "center", alignItems:"center", width: "100%"}}
-          className={`${location.pathname === "/rezultate" ? "md:flex-row lg:flex-row":""}`}>
-      <div className={`flex items-center justify-beetween flex-col mt-5 gap-2 lg:gap-0 lg:flex-row
-          ${location.pathname === "/" ? "w-[100%] mt-0 md:flex-col sm:items-center sm:flex-col sm:items-center" : ""}
-          ${location.pathname === "/rezultate" ? "md:justify-center  w-[80%] md:w-[80%] lg:justify-between 2xl:w-[80%]" : ""}`}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%"
+      }}
+      className={`${
+        location.pathname === "/rezultate" ? "md:flex-row lg:flex-row" : ""
+      }`}
+    >
+      <div
+        className={`flex items-center justify-beetween flex-col mt-5 gap-2 lg:gap-0 lg:flex-row
+          ${
+            location.pathname === "/"
+              ? "w-[100%] mt-0 md:flex-col sm:items-center sm:flex-col sm:items-center"
+              : ""
+          }
+          ${
+            location.pathname === "/rezultate"
+              ? "md:justify-center  w-[80%] md:w-[80%] lg:justify-between 2xl:w-[80%]"
+              : ""
+          }`}
       >
-          {location.pathname === "/rezultate" && (
-            <a href="/" className="logo lg:mr-3" style={{minWidth: "54px"}}>
-              <img src={logo} alt="peviitor"/>
-            </a>
-          )}
-          <form onSubmit={handleUpdateQ} 
-            className={`flex flex-col items-center relative  lg:justify-between lg:mt-0 lg:gap-0 lg:flex-row  max-w-full
+        {location.pathname === "/rezultate" && (
+          <a href="/" className="logo lg:mr-3" style={{ minWidth: "54px" }}>
+            <img src={logo} alt="peviitor" />
+          </a>
+        )}
+        <form
+          onSubmit={handleUpdateQ}
+          className={`flex flex-col items-center relative  lg:justify-between lg:mt-0 lg:gap-0 lg:flex-row  max-w-full
                 ${location.pathname === "/" ? "gap-2 mt-4 md:gap-2" : ""}
-                ${location.pathname === "/rezultate" ? "w-full gap-1 sm:justify-center sm:w-auto md:justify-center md:items-center md:w-[90%]" : ""}`}
-          >
-            <div className={`flex items-center justify-between rounded-full w-[300px] md:w-[480px] lg:w-[340px] 
-                ${location.pathname === "/" ? "relative xl:w-[485px] " : ""}
-                ${location.pathname === "/rezultate" ? "sm:flex-col sm:w-[400px] md:w-[580px] 2xl:w-[90%] lg:w-[90%]" : ""}`}
-            >
-              {/* Job Title Input */}
-              <div 
-                className={`flex items-center relative w-full border border-[#89969C] bg-white rounded-full h-[54px]
-                ${location.pathname === "/rezultate" ? "w-full" : ""}
-                ${location.pathname !== "/"
-                    ? "lg:border-r-2 border-[#89969C] " // border-ul pe dreapta se păstrează pe orice pagină, nu doar pe "/"
-                    : "lg:border-r-0 lg:rounded-tr-none lg:rounded-br-none divider" // Adaugă divider doar pe paginile care nu sunt "/"
-                } 
-                ${focusedInput === "jobTitle" && location.pathname === "/"
-                    ? "lg:border-b-[#eeeeee] lg:rounded-bl-none" 
+                ${
+                  location.pathname === "/rezultate"
+                    ? "w-full gap-1 sm:justify-center sm:w-auto md:justify-center md:items-center md:w-[90%]"
                     : ""
                 }`}
-              >
-                <FlagMagnifyGlass className="ml-5"/>
-                <input
-                  type="text"
-                  value={text}
-                  onChange={(e) => setText(e.target.value)}
-                  onFocus={() => handleFocus("jobTitle")}
-                  placeholder="Caută un loc de muncă"
-                  className="w-full py-3 px-2 xl:pl-2 bg-transparent outline-none border-none focus:outline-none focus:ring-0"
+        >
+          <div
+            className={`flex items-center justify-between rounded-full w-[300px] md:w-[480px] lg:w-[340px] 
+                ${location.pathname === "/" ? "relative xl:w-[485px] " : ""}
+                ${
+                  location.pathname === "/rezultate"
+                    ? "sm:flex-col sm:w-[400px] md:w-[580px] 2xl:w-[90%] lg:w-[90%]"
+                    : ""
+                }`}
+          >
+            {/* Job Title Input */}
+            <div
+              className={`flex items-center relative w-full border border-[#89969C] bg-white rounded-full h-[54px]
+    ${location.pathname === "/rezultate" ? "w-full" : ""}
+    ${
+      location.pathname !== "/"
+        ? "lg:border-r-2 border-[#89969C] " // Border pe dreapta pe orice pagină, nu doar pe "/"
+        : "lg:border-r-0 lg:rounded-tr-none lg:rounded-br-none divider" // Adaugă divider doar pe paginile care nu sunt "/"
+    } 
+    ${
+      focusedInput === "jobTitle" &&
+      text.length >= 3 &&
+      location.pathname === "/"
+        ? "lg:border-b-[#eeeeee] lg:rounded-bl-none"
+        : ""
+    }`}
+            >
+              <FlagMagnifyGlass className="ml-5" />
+              <input
+                type="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                onFocus={() => handleFocus("jobTitle")}
+                placeholder="Caută un loc de muncă"
+                className="w-full py-3 px-2 xl:pl-2 bg-transparent outline-none border-none focus:outline-none focus:ring-0"
+              />
+              {text && (
+                <CloseIcon
+                  className="w-4 h-4 mr-6 fill-slate-500 cursor-pointer"
+                  onClick={handleCloseIcon}
                 />
-                {text && (
-                  <CloseIcon
-                    className="w-4 h-4 mr-6 fill-slate-500 cursor-pointer"
-                    onClick={handleCloseIcon}
-                  />
-                )}
-              </div>
+              )}
+            </div>
 
-              {/* Dropdown for Job Title */}
-              {location.pathname === "/" && focusedInput === "jobTitle" && (
-                <ul 
-                  className="hidden lg:block lg:absolute lg:left-0 lg:w-full lg:border lg:border-t-0 border-[#89969C] lg:rounded-3xl lg:rounded-t-none p-0 lg:mt-4 lg:max-h-[150px] lg:overflow-y-scroll custom-scrollbar lg:bottom-0 lg:transform lg:translate-y-full lg:box-border">
+            {/* Dropdown for Job Title */}
+            {location.pathname === "/" &&
+              focusedInput === "jobTitle" &&
+              text.length >= 3 && (
+                <ul className="hidden lg:block lg:absolute lg:left-0 lg:w-full lg:border lg:border-t-0 border-[#89969C] lg:rounded-3xl lg:rounded-t-none p-0 lg:mt-4 lg:max-h-[150px] lg:overflow-y-scroll custom-scrollbar lg:bottom-0 lg:transform lg:translate-y-full lg:box-border">
+                  {jobSuggestions &&
+                    jobSuggestions.length > 0 &&
+                    jobSuggestions.map((suggestion, index) => (
+                      <li
+                        key={index}
+                        className={`px-12 py-2 cursor-pointer ${
+                          index % 2 === 0 ? "bg-custom-gray" : "bg-white"
+                        } hover:bg-gray-200`}
+                        onMouseDown={() => {
+                          setText(suggestion.term);
+                          setFocusedInput(null);
+                        }}
+                      >
+                        {suggestion.term}
+                      </li>
+                    ))}
+                </ul>
+              )}
+
+            {/* Dropdown for Job Title */}
+            {location.pathname === "/" &&
+              focusedInput === "jobTitle" &&
+              text.length >= 3 && (
+                <ul className="hidden lg:block lg:absolute lg:left-0 lg:w-full lg:border lg:border-t-0 border-[#89969C] lg:rounded-3xl lg:rounded-t-none p-0 lg:mt-4 lg:max-h-[150px] lg:overflow-y-scroll custom-scrollbar lg:bottom-0 lg:transform lg:translate-y-full lg:box-border">
                   {}
                   {jobSuggestions &&
                     jobSuggestions.length > 0 &&
@@ -342,87 +399,88 @@ const Search = () => {
                     ))}
                 </ul>
               )}
-            </div>
+          </div>
 
-            {/* Add Location Input */}
-            <div ref={dropdownRef} >
-              {" "}
-              {/* Add ref to the container */}
-              {location.pathname === "/" && (
+          {/* Add Location Input */}
+          <div ref={dropdownRef}>
+            {" "}
+            {/* Add ref to the container */}
+            {location.pathname === "/" && (
+              <div className="flex items-center justify-between w-[300px] mt-1 relative md:w-[480px] lg:w-[241px] lg:mt-0">
                 <div
-                  className="flex items-center justify-between w-[300px] mt-1 relative md:w-[480px] lg:w-[241px] lg:mt-0">
-                  <div style={{height: "54px" }}
-                    className={`flex items-center  relative w-full border border-[#89969C] bg-white rounded-full  lg:border-l-0 lg:rounded-tl-none lg:rounded-bl-none
+                  style={{ height: "54px" }}
+                  className={`flex items-center  relative w-full border border-[#89969C] bg-white rounded-full  lg:border-l-0 lg:rounded-tl-none lg:rounded-bl-none
                       ${
                         focusedInput === "location"
                           ? "lg:border-b-[#eeeeee] lg:rounded-br-none"
                           : ""
                       }`}
-                  >
-                    <MapPinIcon className="w-6 h-6 text-gray-500 ml-5" />
-                    <input
-                      type="text"
-                      value={isLocation}
-                      onChange={(e) => setLocation(e.target.value)}
-                      onFocus={() => handleFocus("location")}
-                      placeholder="Adaugă o locație"
-                      className="w-full py-3 px-4 pl-2 bg-transparent outline-none border-none focus:outline-none focus:ring-0"
+                >
+                  <MapPinIcon className="w-6 h-6 text-gray-500 ml-5" />
+                  <input
+                    type="text"
+                    value={isLocation}
+                    onChange={(e) => setLocation(e.target.value)}
+                    onFocus={() => handleFocus("location")}
+                    placeholder="Adaugă o locație"
+                    className="w-full py-3 px-4 pl-2 bg-transparent outline-none border-none focus:outline-none focus:ring-0"
+                  />
+                  {isLocation && (
+                    <CloseIcon
+                      className="w-4 h-4 mr-6 fill-slate-500 cursor-pointer"
+                      onClick={handleClearLocation}
                     />
-                    {isLocation && (
-                      <CloseIcon
-                        className="w-4 h-4 mr-6 fill-slate-500 cursor-pointer"
-                        onClick={handleClearLocation}
-                      />
-                    )}
-                  </div>
-
-                  {/* Add Location Input dropdown*/}
-                  {focusedInput === "location" && (
-                    <ul className="hidden lg:block lg:absolute lg:left-0 lg:w-full lg:border lg:border-t-0 lg:border-[#89969C] 
-                    lg:rounded-3xl lg:rounded-t-none  lg:mt-4 lg:max-h-[150px] lg:overflow-y-scroll custom-scrollbar lg:bottom-0 lg:transform lg:translate-y-full lg:box-border">
-                      {filteredCities.map((suggestion, index) => (
-                        <li
-                          key={index}
-                          className={`px-12 py-2 cursor-pointer ${
-                            index % 2 === 0 ? "bg-custom-gray" : "bg-white"
-                          } hover:bg-gray-200`}
-                          onClick={() => {
-                            setLocation(suggestion);
-                            setFocusedInput(null); // Close dropdown on selection
-                          }}
-                        >
-                          {suggestion}
-                        </li>
-                      ))}
-                    </ul>
                   )}
-
                 </div>
-              )}
-            </div>
-                          
-            <Button type="submit" buttonType="search">
-              Caută
-            </Button>
-          </form>
+
+                {/* Add Location Input dropdown*/}
+                {focusedInput === "location" && (
+                  <ul
+                    className="hidden lg:block lg:absolute lg:left-0 lg:w-full lg:border lg:border-t-0 lg:border-[#89969C] 
+                    lg:rounded-3xl lg:rounded-t-none  lg:mt-4 lg:max-h-[150px] lg:overflow-y-scroll custom-scrollbar lg:bottom-0 lg:transform lg:translate-y-full lg:box-border"
+                  >
+                    {filteredCities.map((suggestion, index) => (
+                      <li
+                        key={index}
+                        className={`px-12 py-2 cursor-pointer ${
+                          index % 2 === 0 ? "bg-custom-gray" : "bg-white"
+                        } hover:bg-gray-200`}
+                        onClick={() => {
+                          setLocation(suggestion);
+                          setFocusedInput(null); // Close dropdown on selection
+                        }}
+                      >
+                        {suggestion}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            )}
+          </div>
+
+          <Button type="submit" buttonType="search">
+            Caută
+          </Button>
+        </form>
       </div>
 
       {/* new component */}
-      {location.pathname === "/rezultate" &&
+      {location.pathname === "/rezultate" && (
         <div>
           {location.pathname === "/rezultate" && <FiltreGrup />}
           {loading ? (
-              <div className="h-[20px] w-[50%] md:w-[16%] mx-auto my-8 md:mx-0 bg-gray-300 animate-pulse rounded-md"></div>
-              ) : (
-                total > 0 && (
-                  <h2
-                    className="text-start text-text_grey_darker my-8 text-lg"
-                    style={{ width: h2Width, margin: "32px auto" }}
-                  >
-                    {total} {nrJoburi} 
-                  </h2>
-                )
-              )}
+            <div className="h-[20px] w-[50%] md:w-[16%] mx-auto my-8 md:mx-0 bg-gray-300 animate-pulse rounded-md"></div>
+          ) : (
+            total > 0 && (
+              <h2
+                className="text-start text-text_grey_darker my-8 text-lg"
+                style={{ width: h2Width, margin: "32px auto" }}
+              >
+                {total} {nrJoburi}
+              </h2>
+            )
+          )}
 
           {!deletAll && total > 0 && (
             <div
@@ -441,7 +499,7 @@ const Search = () => {
             </div>
           )}
         </div>
-      }
+      )}
     </div>
   );
 };
