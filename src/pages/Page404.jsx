76df89bb940714133e5d94a5e263a5e9b65170b5
AdelinaMoroaca <moroacaadelina@gmail.com>
2025-02-19 09:35:40 +0200

    Implement basic structure for custom 404 Page

---
+++ src/pages/Page404.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Page404 = () => {
+    return (
+        <Layout>
+            {/* Page Content */}
+            <h1>Oooops page not found</h1>
+        </Layout>
+    );
+};
+
+export default Page404;
