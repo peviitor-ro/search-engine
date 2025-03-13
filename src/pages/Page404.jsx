3d41ec482804ce4d1af4a08ae532f2b66d820599
valentinciolofan <valentincatalin02@gmail.com>
2025-03-13 08:19:22 +0200

    404 page based on figma design

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
+// Import components
 import Layout from "../components/Layout";
+import Button from "../components/Button";
+
+// Import 404 image
+import image404 from "../assets/svg/404image.svg";
+
 
 const Page404 = () => {
+    const navigate = useNavigate();
+
+    const handleBackBtn = () => {
+        navigate("/");
+    }
+
     return (
         <Layout>
             {/* Page Content */}
-            <h1>Oooops page not found</h1>
+            <div className="flex flex-col items-center justify-center w-[80%] gap-6 mt-8 text-center">
+                <img
+                    className="max-w-full h-auto"
+                    src={image404}
+                    alt="Imagine pagină 404"
+                />
+                <p className="font-semibold text-base md:max-w-[750px] md:text-xl leading-7 text-text_grey_darker md:leading-8 tracking-[1.2px]">Se pare că ai ajuns într-un loc unde job-urile se ascund. Dar nu te lăsa descurajat, te ajutăm să le găsești!</p>
+                <Button
+                    type="button"
+                    buttonType={"search"}
+                    onClick={handleBackBtn}
+                    className={"font-medium"}
+                >
+                    Înapoi
+                </Button>
+            </div>
         </Layout>
     );
 };
