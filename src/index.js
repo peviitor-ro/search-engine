94322f83a37447ec34c0ca4b785417f351d3e4a3
Andrei Barari <b.andrei91@yahoo.com>
2025-06-06 09:09:14 +0300

    Integrate and configured sentry

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./sentry-init";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
---
+++ src/sentry-init.js
@@ -0,0 +1,9 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://8c2dc5026cd29be9a3ba8247acdf0fe2@o4509076922499072.ingest.de.sentry.io/4509440197263440",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  environment: process.env.NODE_ENV // group errors by env
+});
