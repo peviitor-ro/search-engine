7138f4d25c55ffe9cc05a6a6821ccf991c51be32
Andrei Barari <b.andrei91@yahoo.com>
2025-06-06 09:36:31 +0300

    Updated sentry config with app version, better displays error per release

--- src/sentry-init.js
+++ src/sentry-init.js
@@ -1,9 +1,11 @@
 import * as Sentry from "@sentry/react";
+import packageJson from "../package.json";
 
 Sentry.init({
   dsn: "https://8c2dc5026cd29be9a3ba8247acdf0fe2@o4509076922499072.ingest.de.sentry.io/4509440197263440",
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
+  release: packageJson.version,
   environment: process.env.NODE_ENV // group errors by env
 });
