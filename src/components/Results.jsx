589619a91b25005c6cf4befd07622883f41e1a40
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2025-06-20 17:19:11 +0300

    Fix: mobile padding overflow and improved images

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -73,7 +73,7 @@ const Results = () => {
       ) : (
         <>
           {jobs.length > 0 ? (
-              <section className="grid gap-7 px-14 w-full mx-auto pb-12  md:flex md:flex-wrap md:justify-center">
+              <section className="grid gap-7 md:px-14 w-full mx-auto pb-12 md:flex md:flex-wrap md:justify-center">
             {/* // <section className="grid gap-7 px-14 w-full mx-auto pb-12 mt-2 md:flex md:flex-wrap md:justify-center"> */}
               {jobs.map(
                 (
